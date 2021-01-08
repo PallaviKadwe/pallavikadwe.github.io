b8512c78667d8e0af742a284520470f846db86f2
PallaviKadwe <kadwepallavi@johndeere.com>
2021-01-08 08:43:39 -0600

    Made changes to the layout

--- script.js
+++
@@ -1,5 +0,0 @@
-function displayAbout(){
-    document.getElementById("mainDiv").innerHTML = '';
-    document.getElementById("mainDiv").innerHTML = "<p>Highly motivated, accomplished, result oriented individual with experience working for global applications in a fortune 500 company  </p>"
-
-} 
